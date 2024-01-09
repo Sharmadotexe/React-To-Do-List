e1a36563e434727563d7a92bf09b883613c10f33
Sharmadotexe <rockvikas300@gmail.com>
2024-01-09 22:34:29 +0530

    added hooks

--- myapp/src/component/todo/todo.js
+++ myapp/src/component/todo/todo.js
@@ -1,7 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './style.css'
 
 const Todo = () => {
+
+
+
+
+    const[inputdata,setinputdata] = useState("")
+    const[item,setitem] = useState([])
+
+
+    const addItem = ()=>{
+        if(!inputdata){
+            alert('fill the input data')
+        }
+
+        else{
+            setitem([...item,inputdata])
+        }
+        }
+    
+
+
+
+
+
+
     return (
         <>
             <div className='main-div'>
@@ -15,10 +39,20 @@ const Todo = () => {
                         <input
                             type='text'
                             placeholder='add your task here'
-                            className='form-control'>
-                        </input>
-                        <i className="fa fa-plus add-btn"></i>
+                            className='form-control'
+                                value ={inputdata}
+                                onChange={(event)=>setinputdata(event.target.value)}
+                        />
+                        <i className="fa fa-plus add-btn" onClick={addItem} ></i>
                         <div className='showItems'>
+                            <div className='eachItem'>
+                                <h3>mango</h3>
+                                <div className='todo-btn'>
+                                    <i className="far fa-edit add-btn"></i>
+                                    <i className="far fa-trash-alt add-btn"></i>
+                                </div>
+                            </div>
+
                             <button className='btn effect04' data-sm-link-text='Remove All'>
                                 <span>
                                     Check list
